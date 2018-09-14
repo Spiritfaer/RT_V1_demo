da3ea283528481db3d5a7a2ee0dee388d8318a5a
Igor <istalevs@student.unit.ua>
2018-09-14 12:15:39 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/RT_V1_demo.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/istalevs/CLionProjects/RT_V1_demo/main.c" "/Users/istalevs/CLionProjects/RT_V1_demo/cmake-build-debug/CMakeFiles/RT_V1_demo.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libft"
+  "../src"
+  "/Users/istalevs/.brew/Cellar/sdl2/2.0.8/include/SDL2"
+  "/Users/istalevs/.brew/Cellar/sdl2_image/2.0.3/include/SDL2"
+  "/Users/istalevs/.brew/Cellar/sdl2_mixer/2.0.2_3/include/SDL2"
+  "/Users/istalevs/.brew/Cellar/sdl2_ttf/2.0.14/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RT_V1_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RT_V1_demo.dir/main.c.o"
+  "RT_V1_demo.pdb"
+  "RT_V1_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RT_V1_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
